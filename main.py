dcdb779003fff63a60d8b75cf881c9fc589b7262
kmilewczyk96 <kmilew0802@gmail.com>
2020-08-27 14:09:27 +0200

    cosmetic changes

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ pygame.display.set_caption('H A N _ _ A N')
 FPS = 60
 clock = pygame.time.Clock()
 
+# style:
 WHITE = (243, 243, 243)
+GREEN_TABLE = (51, 67, 42)
 BLACK = (5, 5, 5)
+LETTER_FONT = pygame.font.SysFont('monospace', 40, bold=True)
+
 alphabet = [i for i in range(97, 123)]
 
 RADIUS = 30
@@ -18,22 +22,21 @@ GAP = 15
 letters = []
 x_pos = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
 y_pos = round(3/4 * HEIGHT)
-a = 65
 for i in range(26):
     x = x_pos + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
     y = y_pos + ((i // 13) * (GAP + RADIUS * 2))
-    letters.append([x, y, chr(a + i)])
-
-LETTER_FONT = pygame.font.SysFont('comicsans', 40)
+    letters.append([x, y, chr(65 + i)])
 
 
 def in_game_graphic():
-    win.fill(WHITE)
+    win.fill(GREEN_TABLE)
+    win.blit(LETTER_FONT.render('Used letters:', 1, WHITE), (200, 3/5 * HEIGHT + 10))
     for letter in letters:
         x, y, ltr = letter
-        pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
-        text = LETTER_FONT.render(ltr, 1, BLACK)
-        win.blit(text, (x - text.get_width() / 2, y - text.get_width() / 2))
+        pygame.draw.circle(win, WHITE, (x, y), RADIUS, 3)
+        pygame.draw.line(win, WHITE, (0, 3/5 * HEIGHT), (WIDTH, 3/5 * HEIGHT), 3)
+        text = LETTER_FONT.render(ltr, 1, WHITE)
+        win.blit(text, (x - text.get_width() / 2, y - text.get_width()))
     pygame.display.update()
 
 
